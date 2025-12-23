4925761d28a8932f9a92e3264c900a5d58985abb
valravn <valravn@demersal.deep.loc>
2025-12-23 08:09:45 -0500

    Added check for hex dumps in TINS db and added more direct file paths

--- rhel-flavored/sql-bkp-cleanup.sh
+++ rhel-flavored/sql-bkp-cleanup.sh
@@ -28,12 +28,12 @@ find "$HOME"/db-backups/ -type f -empty -delete
 
 # Check for the five most recent HALLOWEEN database backup files
 # Keep those files and remove the older HALLOWEEN database backup files
-halloween_bkp_count=$(find "$HOME"/db-backups/mysql/ -type f -name '*halloween*' | sort -r | sed '3,$d' | wc -l)
+halloween_bkp_count=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*halloween*' | sort -r | sed '3,$d' | wc -l)
 
 if [[ "$halloween_bkp_count" -lt 2 ]]; then
   printf "There are not enough Halloween SQL backup files to go over.\n"
 else
-  remove_bkps=$(find "$HOME"/db-backups/mysql/ -type f -name '*halloween*' -print | sort -r | sed '1,2d')
+  remove_bkps=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*halloween*' -print | sort -r | sed '1,2d')
   for bkp in $remove_bkps; do
     rm "$bkp"
   done
@@ -42,26 +42,37 @@ fi
 
 # Check for the five most recent TINS database backup files
 # Keep those files and remove the older TINS database backup files
-sardine_bkp_count=$(find "$HOME"/db-backups/mysql/ -type f -name '*tins*' | sort -r | sed '3,$d' | wc -l)
+sardine_bkp_count=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*tins*' | sort -r | sed '3,$d' | wc -l)
+sardine_hex_count=$(find "$HOME"/db-backups/mysql/hex-dumps/ -type f -name '*tins*' | sort -r | sed '3,$d' | wc -l)
 
 if [[ "$sardine_bkp_count" -lt 2 ]]; then
-  printf "There are not enough Tins SQL backup files to go over.\n"
+  printf "There are not enough Tins SQL STANDARD backup files to go over.\n"
 else
-  remove_bkps=$(find "$HOME"/db-backups/mysql/ -type f -name '*tins*' | sort -r | sed '1,2d')
+  remove_bkps=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*tins*' | sort -r | sed '1,2d')
   for bkp in $remove_bkps; do
     rm "$bkp"
   done
   echo "2 most recent TINS database STANDARD backup files have been preserved.  All others have been removed."
 fi
 
+if [[ "$sardine_hex_count" -lt 2 ]]; then
+  printf "There are not enough Tins SQL HEX backup files to go over.\n"
+else
+  remove_hex=$(find "$HOME"/db-backups/mysql/hex-dumps/ -type f -name '*tins*' | sort -r | sed '1,2d')
+  for hex in $remove_hex; do
+    rm "$hex"
+  done
+  echo "2 most recent TINS database HEX backup files have been preserved.  All others have been removed."
+fi
+
 # Check for the five most recent FITNESS database backup files
 # Keep those files and remove the older FITNESS database backup files
-fitness_bkp_count=$(find "$HOME"/db-backups/mariadb/ -type f -name '*fitness*' | sort -r | sed '3,$d' | wc -l)
+fitness_bkp_count=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*fitness*' | sort -r | sed '3,$d' | wc -l)
 
 if [[ "$fitness_bkp_count" -lt 2 ]]; then
   printf "There are not enough FITNESS SQL backup files to go over.\n"
 else
-  remove_bkps=$(find "$HOME"/db-backups/mariadb/ -type f -name '*fitness*' | sort -r | sed '1,2d')
+  remove_bkps=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*fitness*' | sort -r | sed '1,2d')
   for bkp in $remove_bkps; do
     rm "$bkp"
   done
