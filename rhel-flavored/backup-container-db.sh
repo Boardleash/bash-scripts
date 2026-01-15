8182e3abec7c7d80236cc7c01d20e1f2d1341806
valravn <valravn@demersal.deep.loc>
2026-01-15 08:18:50 -0500

    Consolidated some echo statements

--- rhel-flavored/backup-container-db.sh
+++ rhel-flavored/backup-container-db.sh
@@ -17,65 +17,53 @@ fitness=$(podman ps | grep 'fitness_maria')
 sardines=$(podman ps | grep 'sardine_mysql')
 seamanship=$(podman ps | grep 'seamanship_db')
 
-# If/else condition to check if either the Halloween, Sardine, or Fitness container is online; if so, create a backup of the applicable SQL 
-# container
+# If/else condition to check if either the Halloween, Tins(Sardine), Seamanship or Fitness container is online; if so, create a backup 
+# of the applicable SQL container
 if [[ "$hallow" ]]; then
-  echo "Backing up HALLOWEEN database now..."
+  echo "Backing up Halloween database now..."
   sleep 1
   podman exec -it halloween_mysql mysqldump --set-gtid-purged=OFF halloween > "$HOME"/db-backups/mysql/standard-dumps/"$(date +%F)"-halloween.sql
   podman exec -it halloween_mysql mysqldump --hex-blob halloween > "$HOME"/db-backups/mysql/hex-dumps/"$(date +%F)"-halloween-hex.sql
   rm "$HOME"/containers/halloween/mysql/volumes/*
   cp "$HOME"/db-backups/mysql/standard-dumps/"$(date +%F)"-halloween.sql "$HOME"/containers/halloween/mysql/volumes/
-  echo "HALLOWEEN database backup complete."
-  echo "New HALLOWEEN SQL file has been placed in container volume."
-  echo "Restarting HALLOWEEN MySQL container now..."
+  echo "Halloween database backup complete.  New SQL file has been placed in container volume.  Restarting Halloween MySQL container now..."
   sleep 1
   podman restart halloween_mysql
-  echo "HALLOWEEN MySQL container has been restarted."
-  echo "All done!"
+  echo "Halloween MySQL container has been restarted.  All done!"
 elif [[ "$sardines" ]]; then
-  echo "Backing up TINS database now..."
+  echo "Backing up Tins database now..."
   sleep 1
   podman exec -it sardine_mysql mysqldump --set-gtid-purged=OFF tins > "$HOME"/db-backups/mysql/standard-dumps/"$(date +%F)"-tins.sql
   podman exec -it sardine_mysql mysqldump --hex-blob tins > "$HOME"/db-backups/mysql/hex-dumps/"$(date +%F)"-tins-hex.sql
   rm "$HOME"/containers/sardines/mysql/volumes/*
   cp "$HOME"/db-backups/mysql/standard-dumps/"$(date +%F)"-tins.sql "$HOME"/containers/sardines/mysql/volumes/
-  echo "TINS database backup complete."
-  echo "New TINS SQL file has been placed in container volume."
-  echo "Restarting SARDINE MySQL container now..."
+  echo "Tins database backup complete.  New SQL file has been placed in container volume.  Restarting SARDINE MySQL container now..."
   sleep 1
   podman restart sardine_mysql
-  echo "SARDINE MySQL container has been restarted."
-  echo "All done!"
+  echo "Tins MySQL container has been restarted.  All done!"
 elif [[ "$fitness" ]]; then
-  echo "Backing up FITNESS database now..."
+  echo "Backing up Fitness database now..."
   sleep 1
   podman exec -it fitness_maria mariadb-dump fitness > "$HOME"/db-backups/mariadb/standard-dumps/"$(date +%F)"-fitness.sql
   rm "$HOME"/containers/fitness/mariadb/volume/*
   cp "$HOME"/db-backups/mariadb/standard-dumps/"$(date +%F)"-fitness.sql "$HOME"/containers/fitness/mariadb/volume/
-  echo "FITNESS database backup complete."
-  echo "New FITNESS SQL file has been placed in container volume."
-  echo "Restarting FITNESS MariaDB container now.."
+  echo "Fitness database backup complete.  New SQL file has been placed in container volume.  Restarting Fitness MariaDB container now..."
   sleep 1
   podman restart fitness_maria
-  echo "FITNESS MariaDB container has been restarted."
-  echo "All done!"
+  echo "Fitness MariaDB container has been restarted.  All done!"
 elif [[ "$seamanship" ]]; then
-  echo "Backing up SEAMANSHIP database now..."
+  echo "Backing up Seamanship database now..."
   sleep 1
   podman exec -it seamanship_db mariadb-dump study > "$HOME"/db-backups/mariadb/standard-dumps/"$(date +%F)"-seamanship.sql
   rm "$HOME"/containers/seamanship/mariadb/volume/*
   cp "$HOME"/db-backups/mariadb/standard-dumps/"$(date +%F)"-seamanship.sql "$HOME"/containers/seamanship/mariadb/volume/
-  echo "SEAMANSHIP database backup complete."
-  echo "New SEAMANSHIP SQL file has been placed in container volume."
-  echo "Restarting SEAMANSHIP MariaDB container now.."
+  echo "Seamanship database backup complete.  New SQL file has been placed in container volume.  Restarting Seamanship MariaDB container now..."
   sleep 1
   podman restart seamanship_db 
   sleep 1
-  echo "SEAMANSHIP MariaDB container has been restarted."
-  echo "All done!"
+  echo "Seamanship MariaDB container has been restarted.  All done!"
 else
-  echo "Unable to create backup; SQL container is not online."
+  echo "Unable to create backup; database container is not online."
 fi
 
 # EOF
