2a79d61a48c7f12f26b15e8eaab01f28736e134f
valravn <valravn@demersal.deep.loc>
2026-03-07 05:39:58 -0500

    Added code for other DB dumps; minor comment changes

--- rhel-flavored/sql-bkp-cleanup.sh
+++ rhel-flavored/sql-bkp-cleanup.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-########################################
+#-----------------------------------
 # TITLE: sql-backup-cleanup.sh
 # AUTHOR: Boardleash
 # DATE: Tuesday, October 14 2025
-#####################################
+#-----------------------------------
 
-# This is a cleanup script for database backup files as they start to take up space
+#------------------ DESCRIPTION ------------------
+# Cleanup script for database backup files as they 
+# start to take up space.
+#-------------------------------------------------
 
-# Check if there is a cleanup-stats file already created.  If not, create one, otherwise,
-# append a space to it to start the new cleanup stats
+# Check for stats file; create one if needed or append to one that already exists
 check_file="$HOME/sql-cleanup-stats"
 if [ ! -e "$check_file" ]; then
   touch "$HOME"/sql-cleanup-stats
@@ -25,9 +27,7 @@ deleted_files=$(find "$HOME"/db-backups/ -type f -empty | wc -l)
 echo "$(date +%c) EMPTY FILES DELETED: $deleted_files" >> "$check_file"
 find "$HOME"/db-backups/ -type f -empty -delete
 
-
-# Check for the five most recent HALLOWEEN database backup files
-# Keep those files and remove the older HALLOWEEN database backup files
+# Check for five most recent HALLOWEEN database backup files, keep them and remove older ones
 halloween_bkp_count=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*halloween*' | sort -r | sed '3,$d' | wc -l)
 
 if [[ "$halloween_bkp_count" -lt 2 ]]; then
@@ -37,36 +37,34 @@ else
   for bkp in $remove_bkps; do
     rm "$bkp"
   done
-  echo "2 most recent HALLOWEEN STANDARD database backup files have been preserved.  All others have been removed."
+  echo "2 most recent HALLOWEEN database backup files have been saved.  All others have been removed."
 fi
 
-# Check for the five most recent TINS database backup files
-# Keep those files and remove the older TINS database backup files
+# Check for five most recent TINS database backup files, keep them and remove older ones
 sardine_bkp_count=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*tins*' | sort -r | sed '3,$d' | wc -l)
 sardine_hex_count=$(find "$HOME"/db-backups/mysql/hex-dumps/ -type f -name '*tins*' | sort -r | sed '3,$d' | wc -l)
 
 if [[ "$sardine_bkp_count" -lt 2 ]]; then
-  printf "There are not enough Tins SQL STANDARD backup files to go over.\n"
+  printf "There are not enough TINS SQL STANDARD backup files to go over.\n"
 else
   remove_bkps=$(find "$HOME"/db-backups/mysql/standard-dumps/ -type f -name '*tins*' | sort -r | sed '1,2d')
   for bkp in $remove_bkps; do
     rm "$bkp"
   done
-  echo "2 most recent TINS database STANDARD backup files have been preserved.  All others have been removed."
+  echo "2 most recent Tins database STANDARD backup files have been saved.  All others have been removed."
 fi
 
 if [[ "$sardine_hex_count" -lt 2 ]]; then
-  printf "There are not enough Tins SQL HEX backup files to go over.\n"
+  printf "There are not enough TINS SQL HEX backup files to go over.\n"
 else
   remove_hex=$(find "$HOME"/db-backups/mysql/hex-dumps/ -type f -name '*tins*' | sort -r | sed '1,2d')
   for hex in $remove_hex; do
     rm "$hex"
   done
-  echo "2 most recent TINS database HEX backup files have been preserved.  All others have been removed."
+  echo "2 most recent TINS database HEX backup files have been saved.  All others have been removed."
 fi
 
-# Check for the five most recent FITNESS database backup files
-# Keep those files and remove the older FITNESS database backup files
+# Check for five most recent FITNESS database backup files, keep them and remove older ones
 fitness_bkp_count=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*fitness*' | sort -r | sed '3,$d' | wc -l)
 
 if [[ "$fitness_bkp_count" -lt 2 ]]; then
@@ -76,13 +74,36 @@ else
   for bkp in $remove_bkps; do
     rm "$bkp"
   done
-  echo "2 most recent FITNESS database STANDARD backup files have been preserved.  All others have been removed."
+  echo "2 most recent FITNESS database backup files have been saved.  All others have been removed."
 fi
 
-# Check the new amount of space used after file cleanup
+# Check for five most recent TRAILS database backup files, keep them and remove the older ones
+trails_bkp_count=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*trails*' | sort -r | sed '3,$d' | wc -l)
+if [[ "$trails_bkp_count" -lt 2 ]]; then
+  printf "There are not enough TRAILS backup files to go over.\n"
+else
+  remove_bkps=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*trails*' | sort -r | sed '1,2d')
+  for bkp in $remove_bkps; do
+    rm "$bkp"
+  done
+  echo "2 most recent TRAILS database backup files have been saved.  All others have been removed."
+fi
+
+# Check for five most recent EVERYTHING database backup files, keep them and remove the older ones
+everything_bkp_count=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*everything*' | sort -r | sed '3,$d' | wc -l)
+if [[ "$everything_bkp_count" -lt 2 ]]; then
+  printf "There are not enough EVERYTHING backup files to go over.\n"
+else
+  remove_bkps=$(find "$HOME"/db-backups/mariadb/standard-dumps/ -type f -name '*everything*' | sort -r | sed '1,2d')
+  for bkp in $remove_bkps; do
+    rm "$bkp"
+  done
+  echo "2 most recent EVERYTHING database backup files have been saved.  All others have been removed."
+fi
+# Check new amount of space used after file cleanup
 new_space=$(du -s "$HOME"/db-backups/mysql/ | awk '{print $1}')
 
-# Get a number of how much space was saved after doing the cleanup
+# Get number of how much space was saved after cleanup
 saved_space=$(($original_space - $new_space))
 echo "$(date +%c) SPACE PRIOR TO FILE CLEANUP: $original_space bytes" >> "$check_file"
 echo "$(date +%c) SPACE AFTER FILE CLEANUP: $new_space bytes">> "$check_file"
